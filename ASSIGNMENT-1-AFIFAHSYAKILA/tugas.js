0f48af16c4127d3a00ed89a7ba0ac5014d605c83
AfifahSyakila <afifah05112008@gmail.com>
2025-10-05 21:32:55 +0700

    initial commit

---
+++ ASSIGNMENT-1-AFIFAHSYAKILA/tugas.js
@@ -0,0 +1,29 @@
+//penjumlahan
+const tambah = (a,b) => {
+    return a + b;
+}
+console.log(tambah(5,5));
+
+//pengurangan
+const kurang = (a,b) => {
+    return a - b;
+}
+console.log(kurang(5,3));
+
+//perkalian
+const kali = (a,b) => {
+    return a * b;
+}
+console.log(kali(2,4));
+
+//pembagian
+const bagi = (a,b) => {
+    return a / b;
+}
+console.log(bagi(6,2));
+
+//perpangkatan
+const pangkat = (a,b) => {
+    return a ** b;
+}
+console.log(pangkat(2,3))
